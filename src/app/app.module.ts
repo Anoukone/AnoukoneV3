6ae0e0d5fa4ba164eeefc6f64c40227743f24f8e
jonah <jonahspc2@gmail.com>
2022-04-05 14:04:21 +0700

    jo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    ProfileComponent,
+    
   ],
   imports: [
     BrowserModule,
