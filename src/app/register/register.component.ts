efe98433282664c0d690befa6fec55fcf9cf8024
unknown <aiysomoulai@gmail.com>
2022-04-11 13:57:59 +0700

    Aiy commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl,Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+
+
+
+  constructor(private fb: FormBuilder) {
+
   }
 
-}
+  registerForm = new FormGroup({});
+  submitted = false;
+
+
+  ngOnInit(): void {
+    this.registerForm=  this.fb.group({
+      email: ['',[Validators.required,Validators.email]],
+      password: ['',[Validators.required,Validators.minLength(8)]],
+      confirm_password: ['',Validators.required],
+      first_name: ['',Validators.required],
+      last_name: ['',Validators.required],
+      telephone: ['',Validators.required],
+      date_of_birth: ['',[Validators.required,Validators.pattern(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/)]],
+      gender: ['',Validators.required],
+      village: ['',Validators.required],
+      district: ['',Validators.required],
+      province: ['',Validators.required],
+    });
+  }
+
+  get f(){
+    return this.registerForm.controls;
+  }
+
+  Onsubmit(){
+    this.submitted = true;
+    if(this.registerForm.invalid){
+      return;
+    }
+    // console.log(this.registerForm.value)
+  }
+
+  onReset(){
+      this.submitted = false;
+      this.registerForm.reset();
+    }
+
+
+  }
+
+
+
+
